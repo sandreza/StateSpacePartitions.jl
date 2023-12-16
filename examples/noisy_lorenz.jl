e6c4ef103a0595c9fbea4ed136d405f13731b2b1
Andre Souza <andrenogueirasouza@gmail.com>
2023-12-15 20:41:30 -0800

    noisy version

---
+++ examples/noisy_lorenz.jl
@@ -0,0 +1,22 @@
+using StateSpacePartitions, ProgressBars, Random, GLMakie 
+include("chaotic_systems.jl")
+include("timestepping_utils.jl")
+include("visualization_utils.jl")
+Random.seed!(1234)
+
+dt = 0.01 
+iterations = 10^5
+
+trajectory = zeros(3, iterations)
+trajectory[:, 1] .= [14.0, 20.0, 27.0]
+step = RungeKutta4(3)
+for i in ProgressBar(2:iterations)
+    step(lorenz, trajectory[:, i-1], dt)
+    trajectory[:, i] .= step.xⁿ⁺¹ .+ sqrt(dt) * randn(3)
+end
+
+method = Tree(false, 0.001/3)
+state_space_partitions = StateSpacePartition(trajectory; method = method)
+
+
+visualize_koopman_mode(trajectory, state_space_partitions.partitions, colormap1 = :balance, mode = 4)
